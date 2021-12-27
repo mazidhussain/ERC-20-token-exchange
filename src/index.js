9dcbec3a964cc60a4953da43ebd7bddd9a3f5aa6
yogitasinglaa <95430952+yogitasinglaa@users.noreply.github.com>
2021-12-27 21:02:51 +0530

    make it better

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {Provider} from 'react-redux'
+import 'bootstrap/dist/css/bootstrap.css'
+import App from './components/App'
+import configureStore from './store/configureStore'
+import reportWebVitals from './reportWebVitals'
+
+const store=configureStore()
+
+ReactDOM.render(
+  <Provider store={configureStore()}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
+
+reportWebVitals()
